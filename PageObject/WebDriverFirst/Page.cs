9797fe069397d2411e28d0892c6e5dc9520bfea7
pavel0010100101i <pavel.varecz@mail.ru>
2019-11-14 16:45:49 +0300

    Add project PageObject

---
+++ PageObject/WebDriverFirst/Page.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using Docker.DotNet.Models;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+
+namespace PageObject
+{
+    public class Page
+    {
+        protected DefaultWait<IWebDriver> fluentWait;
+        protected IWebDriver driver;
+        protected static int timeoutInSeconds = 30;
+        public IWebElement СonfirmationСancellation { get; private set; }
+        public IWebElement SelectError { get; private set; }
+        protected readonly IWebElement LogIn;
+        protected readonly IWebElement SearchInputCountry;
+        protected readonly IWebElement InputLogin;
+        protected readonly IWebElement InputPassword;
+        protected readonly IWebElement SignInToYourAccount;
+        protected readonly IWebElement AccountOpen;
+        protected readonly IWebElement ReservationItem;
+        protected readonly IWebElement SubmitBookingButton;
+        protected readonly IWebElement SelectCancelThisBooelectCancelRking;
+        protected readonly IWebElement Seservation;
+
+        [Obsolete]
+        public Page(IWebDriver driver)
+        {
+            fluentWait = GetFluentWait(driver);
+            PageFactory.InitElements(driver, this);
+            this.driver = driver;
+        }
+
+        [Obsolete]
+        private static ReadOnlyCollection<IWebElement> WaitForElement(IWebDriver webDriver, By by)
+        {
+            return (new WebDriverWait(webDriver, TimeSpan.FromSeconds(timeoutInSeconds))
+                .Until(ExpectedConditions.PresenceOfAllElementsLocatedBy(by)));
+        }
+
+        public static DefaultWait<IWebDriver> GetFluentWait(IWebDriver driver)
+        {
+            DefaultWait<IWebDriver> fluentWait = new DefaultWait<IWebDriver>(driver);
+            fluentWait.Timeout = TimeSpan.FromSeconds(50);
+            fluentWait.PollingInterval = TimeSpan.FromMilliseconds(250);
+            fluentWait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+            return fluentWait;
+        }
+    }
+}
---
+++ PageObject/WebDriverFirst/TrainpalCancellationOfOrderOfConfirmationPage.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Threading;
+using Docker.DotNet.Models;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using PageObject;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace PageObject
+{
+    class TrainpalCancellationOfOrderOfConfirmationPage : Page
+    {
+        [FindsBy(How = How.ClassName, Using = "LoginButton__label")]
+        private readonly IWebElement LogIn;
+
+        [FindsBy(How = How.ClassName, Using = "floatl__input")]
+        private readonly IWebElement InputLogin;
+
+        [FindsBy(How = How.ClassName, Using = "floatl__input")]
+        private readonly IWebElement InputPassword;
+
+        [FindsBy(How = How.ClassName, Using = "LoginButton__circle")]
+        private readonly IWebElement SignInToYourAccount;
+
+        [FindsBy(How = How.CssSelector, Using = "UserDetails__link")]
+        private readonly IWebElement AccountOpen;
+
+        [FindsBy(How = How.XPath, Using = "//*div[@class='CustomerLayout__quickLinksWrapper']/ul/li[3]/a")]
+        private readonly IWebElement ReservationItem;
+
+        [FindsBy(How = How.ClassName, Using = "ReservationItem__optionsLinkLabel")]
+        private readonly IWebElement SelectCancelThisBooking;
+
+
+        [FindsBy(How = How.CssSelector, Using = "//ul[@class='ReservationDetailsView__actions']/li[2]")]
+        private readonly IWebElement SelectCancelReservation;
+
+        [FindsBy(How = How.ClassName, Using = "Button__buttonContent")]
+        public IWebElement СonfirmationСancellation { get; private set; }
+
+        public TrainpalCancellationOfOrderOfConfirmationPage(IWebDriver driver) : base(driver) { }
+
+        public TrainpalCancellationOfOrderOfConfirmationPage FillLogin(string text)
+        {
+            InputLogin.SendKeys(text);
+            return this;
+        }
+
+        public TrainpalCancellationOfOrderOfConfirmationPage FillPass(string text)
+        {
+            InputPassword.SendKeys(text);
+            return this;
+        }
+
+        public TrainpalCancellationOfOrderOfConfirmationPage Authorization()
+        {
+            LogIn.Click();
+            return this;
+        }
+        public TrainpalCancellationOfOrderOfConfirmationPage EntranceProfile()
+        {
+            SignInToYourAccount.Click();
+            AccountOpen.Click();
+            return this;
+        }
+        public TrainpalCancellationOfOrderOfConfirmationPage SearchReservations()
+        {
+            ReservationItem.Click();
+            SelectCancelThisBooking.Click();
+            return this;
+        }
+        public TrainpalCancellationOfOrderOfConfirmationPage CancelReservations()
+        {
+            SelectCancelReservation.Click();
+            return this;
+        }
+    }
+}
